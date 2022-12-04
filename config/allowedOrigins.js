833c996015efc255b85aaebae2cbf36d448cbbba
daniel38530324 <89857241+daniel38530324@users.noreply.github.com>
2022-12-04 22:09:37 +0800

    server

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -4,7 +4,19 @@ const allowedOrigins = [
     'http://localhost:3000',
     'http://localhost:3000/Login',
     'https://longing-head-production.up.railway.app/Login',
-    'https://longing-head-production.up.railway.app'
+    'https://longing-head-production.up.railway.app',
+    'https://reactproject3client-production.up.railway.app',
+    'https://reactproject3client-production.up.railway.app/',
+    'https://reactproject3client-production.up.railway.app/Article/',
+    'https://reactproject3client-production.up.railway.app/Article2/',
+    'https://reactproject3client-production.up.railway.app/Article/1',
+    'https://reactproject3client-production.up.railway.app/Article/2',
+    'https://reactproject3client-production.up.railway.app/Article/3',
+    'https://reactproject3client-production.up.railway.app/Article/4',
+    'https://reactproject3client-production.up.railway.app/Article2/1',
+    'https://reactproject3client-production.up.railway.app/Article2/2',
+    'https://reactproject3client-production.up.railway.app/Article2/3',
+    'https://reactproject3client-production.up.railway.app/Article2/4',
 ];
 
 module.exports = allowedOrigins;
